81a57753d155d324432d513a28ef03bfdc81dd17
soetas <3228891558@qq.com>
2025-02-27 22:13:35 +0800

    feat: update files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eva',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
